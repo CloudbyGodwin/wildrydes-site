62f4e4fcccf323011b637ce4ef477887557c4308
CloudbyGodwin <cloudbygodwin@gmail.com>
2025-02-18 18:14:32 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_9hrCVJhEW', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6ed4bnd8cktnjp89ghk7gd5jdj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_PgduQl0yW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1vlln07krgtp995viqo2llmc62', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
